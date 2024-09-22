2b80b95d893d2ee360cc572b7d66b4936e4815a3
Matt O'Tousa <mlotousa@gmail.com>
2024-09-21 20:51:59 -0700

    project.godot settings

--- make_addon.sh
+++ make_addon.sh
@@ -11,6 +11,16 @@ cd submodules
 mkdir -p "$1"
 cd "$1"
 git init
+
+echo "config_version=5" >> "project.godot"
+echo "[debug]" >> "project.godot"
+echo "" >> "project.godot"
+echo "gdscript/warnings/exclude_addons=false" >> "project.godot"
+echo "gdscript/warnings/untyped_declaration=1" >> "project.godot"
+echo "gdscript/warnings/unsafe_property_access=1" >> "project.godot"
+echo "gdscript/warnings/unsafe_method_access=1" >> "project.godot"
+echo "gdscript/warnings/return_value_discarded=1" >> "project.godot"
+
 mkdir -p "addons/$2"
 
 cd addons/$2
@@ -31,6 +41,7 @@ echo "  pass" >> "plugin.gd"
 echo "" >> "plugin.gd"
 echo "func _exit_tree():" >> "plugin.gd"
 echo "  pass" >> "plugin.gd"
+
 
 mkdir src
 mkdir media
